9054e15d6a1c326585736544196bd4451fa2f310
LeminYu997 <1445531395@qq.com>
2020-03-06 19:13:33 +0800

    new file

--- chapter_5/5.4.c
+++ chapter_5/5.4.c
@@ -1,4 +1,5 @@
-//散列表——分离链接法——字符类型
+//实例：电话狂人
+//输入一个次数，和每次的电话聊天双方号码，得到一个打电话次数最多的号码
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -6,7 +7,7 @@
 #include <string.h>
 
 #define MAXTABLESIZE 100000                 //允许开辟的最大散列表长度
-#define KEYLENGTH 15                        //关键字符串的最大长度
+#define KEYLENGTH 11                        //关键字符串的最大长度
 typedef char ElementType[KEYLENGTH + 1];    //关键词类型用字符串
 typedef int Index;                          //散列地址类型
 
@@ -15,6 +16,7 @@ typedef struct LNode *PtrToLNode;
 struct LNode {
     ElementType Data;
     PtrToLNode Next;
+    int Count;                              //电话出现的次数
 };
 typedef PtrToLNode Position;
 typedef PtrToLNode List;
@@ -26,13 +28,41 @@ struct TblNode {                            //散列表节点定义
     List Heads;                             //指向链表头节点的数组
 };
 
-//移位法散列函数
-Index Hash(const char *Key, int TableSize)
+int Hash(int Key, int P);
+int NextPrime(int N);
+HashTable CreateTable(int TableSize);
+Position Find(HashTable H, ElementType Key);
+bool Insert(HashTable H, ElementType Key);
+void DestroyTable(HashTable H);
+void ScanAnOutPut(HashTable H);
+
+//主体
+int main()
 {
-    unsigned int h = 0;     //散列函数值，初始化为0
-    while (*Key != '\0')    //位移映射
-        h = (h << 5) + *Key++;
-    return h % TableSize;
+    //一、创建散列表
+    int N, i;
+    ElementType Key;
+    HashTable H;
+
+    scanf("%d", &N);
+    H = CreateTable(N * 2);                 //创建一个散列表
+    for (i = 0; i < N; i++)
+    {
+        scanf("%s", Key);
+        Insert(H, Key);
+        scanf("%s", Key);
+        Insert(H, Key);
+    }
+    ScanAnOutPut(H);
+    DestroyTable(H);
+    
+    return 0;
+}
+
+//除留余数法
+int Hash(int Key, int P)
+{
+    return Key % P;
 }
 
 //返回大于N且不超过MAXTABLESIZE的最小素数
@@ -73,6 +103,7 @@ HashTable CreateTable(int TableSize)
     {
         H->Heads[i].Data[0] = '\0';
         H->Heads[i].Next = NULL;
+        H->Heads[i].Count = 0;
     }
 
     return H;
@@ -85,7 +116,7 @@ Position Find(HashTable H, ElementType Key)
     Index Pos;
 
     //初始散列位置
-    Pos = Hash(Key, H->TableSize);
+    Pos = Hash(atoi(Key + KEYLENGTH), H->TableSize);
     P = H->Heads[Pos].Next;         //从该链表的第一个节点开始
     //当未到表尾，并且Key未找到时
     while (P && strcmp(P->Data, Key))
@@ -107,7 +138,8 @@ bool Insert(HashTable H, ElementType Key)
     {
         NewCell = (Position)malloc(sizeof(struct LNode));
         strcpy(NewCell->Data, Key);
-        Pos = Hash(Key, H->TableSize);      //初始散列位置
+        NewCell->Count = 1;
+        Pos = Hash(atoi(Key + KEYLENGTH), H->TableSize);      //初始散列位置
         //将NewCell插入为H->Heads[Pos]链表的第一个节点
         NewCell->Next = H->Heads[Pos].Next;
         H->Heads[Pos].Next = NewCell;
@@ -116,7 +148,7 @@ bool Insert(HashTable H, ElementType Key)
     //关键词已存在
     else
     {
-        printf("Key already exists.\n");
+        P->Count++;
         return false;
     }
 }
@@ -140,4 +172,41 @@ void DestroyTable(HashTable H)
     }
     free(H->Heads);             //释放头节点数组
     free(H);                    //释放散列表节点
+}
+
+//输出狂人
+void ScanAnOutPut(HashTable H)
+{
+    int i, MaxCnt = 0;
+    int PCnt = 0;
+    ElementType MinPhone;
+    List Ptr;
+    MinPhone[0] = '\0';
+    //扫描链表
+    for (i = 0; i < H->TableSize; i++)
+    {
+        Ptr = H->Heads[i].Next;
+        while (Ptr)
+        {   
+            //更新最大通话次数
+            if (Ptr->Count > MaxCnt)
+            {
+                MaxCnt = Ptr->Count;
+                strcpy(MinPhone, Ptr->Data);
+                PCnt = 1;
+            }
+            else if (Ptr->Count == MaxCnt)
+            {
+                PCnt++;         //狂人计数
+                //更新狂人的最小手机号码
+                if (strcmp(MinPhone, Ptr->Data) > 0)
+                    strcpy(MinPhone, Ptr->Data);        
+            }
+            Ptr = Ptr->Next;
+        }
+    }
+    printf("%s %d", MinPhone, MaxCnt);
+    if (PCnt > 1)
+        printf(" %d", PCnt);
+    printf("\n");
 }
